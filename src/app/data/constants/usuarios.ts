1da62621c60788fa1da1d9edbea95d4c91dff2b6
Bruno H. Homem <brunohhomem@gmail.com>
2024-06-01 17:39:45 -0300

    feat: added constants

---
+++ src/app/data/constants/usuarios.ts
@@ -0,0 +1,46 @@
+import { Usuario } from '@/core/model/Usuario'
+
+const usuarios: Usuario[] = [
+  {
+    id: '1',
+    email: 'bruno@email.com',
+    nome: 'bruno',
+    senha: '1234',
+    ativo: true
+  },
+  {
+    id: '2',
+    email: 'vit@email.com',
+    nome: 'vitoria',
+    senha: '1234',
+    ativo: true
+  },
+  {
+    id: '3',
+    email: 'butters@email.com',
+    nome: 'butters',
+    senha: '1234',
+    ativo: true
+  },
+  {
+    id: '4',
+    email: 'mione@email.com',
+    nome: 'hermione',
+    senha: '1234',
+    ativo: true
+  },
+  {
+    id: '5',
+    email: 'henrique@email.com',
+    nome: 'henrique',
+    senha: '1234',
+    ativo: true
+  },
+  {
+    id: '6',
+    email: 'brunohhomem@email.com',
+    nome: 'brunohhomem',
+    senha: '1234',
+    ativo: true
+  }
+]
