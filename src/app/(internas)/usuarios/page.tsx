f99cf0f0764d3a97421fa2677272d499c449d369
Bruno H. Homem <brunohhomem@gmail.com>
2024-06-02 15:59:53 -0300

    feat: added titulo

--- src/app/(internas)/usuarios/page.tsx
+++ src/app/(internas)/usuarios/page.tsx
@@ -1,9 +1,16 @@
 import Pagina from '@/app/components/template/Pagina'
+import Titulo from '@/app/components/template/Titulo'
 import ListaUsuario from '@/app/components/usuario/ListaUsuario'
+import { IconUser } from '@tabler/icons-react'
 
 export default function Page() {
   return (
-    <Pagina>
+    <Pagina className="flex flex-col gap-8">
+      <Titulo
+        icone={IconUser}
+        principal="Usuários"
+        secundario="Cadastro de Usuários"
+      />
       <ListaUsuario />
     </Pagina>
   )
--- src/app/components/template/Pagina.tsx
+++ src/app/components/template/Pagina.tsx
@@ -2,13 +2,16 @@ import Menu from './Menu'
 
 export interface PaginaProps {
   children: any
+  className?: string
 }
 
 export default function Pagina(props: PaginaProps) {
   return (
     <div className="flex ">
       <Menu />
-      <main className="p-7 flex-1">{props.children}</main>
+      <main className={`p-7 flex-1 ${props.className ?? ''}`}>
+        {props.children}
+      </main>
     </div>
   )
 }
