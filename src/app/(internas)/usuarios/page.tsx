eeb2527f64bfc41644e0fd187093c78a4eaa138f
Bruno H. Homem <brunohhomem@gmail.com>
2024-06-04 19:07:00 -0300

    feat: added hook

--- src/app/(internas)/usuarios/page.tsx
+++ src/app/(internas)/usuarios/page.tsx
@@ -4,34 +4,11 @@ import Pagina from '@/app/components/template/Pagina'
 import Titulo from '@/app/components/template/Titulo'
 import FormularioUsuario from '@/app/components/usuario/FormularioUsuario'
 import ListaUsuario from '@/app/components/usuario/ListaUsuario'
-import Backend from '@/backend'
-import { Usuario } from '@/core/model/Usuario'
+import useUsuarios from '@/app/data/hooks/useUsuarios'
 import { IconPlus, IconUser } from '@tabler/icons-react'
-import { useEffect, useState } from 'react'
 
 export default function Page() {
-  const [usuarios, setUsuarios] = useState<Usuario[]>([])
-  const [usuario, setUsuario] = useState<Partial<Usuario> | null>(usuarios[0])
-
-  useEffect(() => {
-    Backend.usuarios.obter().then(setUsuarios)
-  }, [])
-
-  async function salvar() {
-    if (!usuario) return
-    await Backend.usuarios.salvar(usuario)
-    const usuarios = await Backend.usuarios.obter()
-    setUsuarios(usuarios)
-    setUsuario(null)
-  }
-
-  async function excluir() {
-    if (!usuario || !usuario.id) return
-    await Backend.usuarios.excluir(usuario.id!)
-    const usuarios = await Backend.usuarios.obter()
-    setUsuarios(usuarios)
-    setUsuario(null)
-  }
+  const { usuarios, usuario, salvar, excluir, alterarUsuario } = useUsuarios()
 
   return (
     <Pagina className="flex flex-col gap-8">
@@ -44,9 +21,9 @@ export default function Page() {
       {usuario ? (
         <FormularioUsuario
           usuario={usuario}
-          onChange={setUsuario}
+          onChange={alterarUsuario}
           salvar={salvar}
-          cancelar={() => setUsuario(null)}
+          cancelar={() => alterarUsuario(null)}
           excluir={excluir}
         />
       ) : (
@@ -54,13 +31,13 @@ export default function Page() {
           <div className="flex justify-end">
             <button
               className="bg-blue-500 px-4 py-2 rounded-md gap-1 flex"
-              onClick={() => setUsuario({})}
+              onClick={() => alterarUsuario({})}
             >
               <IconPlus />
               <span>Novo Usuário</span>
             </button>
           </div>
-          <ListaUsuario usuarios={usuarios} onClick={setUsuario} />
+          <ListaUsuario usuarios={usuarios} onClick={alterarUsuario} />
         </>
       )}
     </Pagina>
